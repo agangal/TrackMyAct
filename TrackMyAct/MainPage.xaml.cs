f7aa3de891a1489ea94647090fdc6358bb0d9446
agangal <ashish.gangal1990@gmail.com>
2016-01-30 19:32:21 -0800

    Basic animations looking good. Next up is the actual timer.

--- TrackMyAct/MainPage.xaml.cs
+++ TrackMyAct/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -11,6 +12,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -28,19 +30,58 @@ namespace TrackMyAct
         }
 
        
+        
+        private void GoEllipse_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Storyboard myStoryboard;
+            Debug.WriteLine("In Go Ellipse Tapped Event");
+            myStoryboard = (Storyboard)this.Resources["GoButtonPressed"];
+            myStoryboard.Begin();
+            GoTextBlock.IsTapEnabled = false;
+            GoEllipse.IsTapEnabled = false;
+            StopEllipse.IsTapEnabled = true;
+            StopTextBlock.IsTapEnabled = true;
+        }
+
+        private void RecycleButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void GoTextBlock_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Storyboard myStoryboard;
+            Debug.WriteLine("In Go Text Tapped Event");
+            myStoryboard = (Storyboard)this.Resources["GoButtonPressed"];
+            myStoryboard.Begin();
+            GoTextBlock.IsTapEnabled = false;
+            GoEllipse.IsTapEnabled = false;
+            StopEllipse.IsTapEnabled = true;
+            StopTextBlock.IsTapEnabled = true;
+        }
+
         private void StopEllipse_Tapped(object sender, TappedRoutedEventArgs e)
         {
-
+            Storyboard myStoryboard;
+            Debug.WriteLine("In Stop Ellipse Tapped Event");
+            myStoryboard = (Storyboard)this.Resources["StopButtonPressed"];
+            myStoryboard.Begin();
+            GoTextBlock.IsTapEnabled = true;
+            GoEllipse.IsTapEnabled = true;
+            StopEllipse.IsTapEnabled = false;
+            StopTextBlock.IsTapEnabled = false;
         }
 
         private void StopTextBlock_Tapped(object sender, TappedRoutedEventArgs e)
         {
-
-        }
-
-        private void GoEllipse_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-
+            Storyboard myStoryboard;
+            Debug.WriteLine("In Stop Text Tapped Event");
+            myStoryboard = (Storyboard)this.Resources["StopButtonPressed"];
+            myStoryboard.Begin();
+            GoTextBlock.IsTapEnabled = true;
+            GoEllipse.IsTapEnabled = true;
+            StopEllipse.IsTapEnabled = false;
+            StopTextBlock.IsTapEnabled = false;
         }
     }
 }
